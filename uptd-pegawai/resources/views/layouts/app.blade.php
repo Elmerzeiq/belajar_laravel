31400830276e52c3c11828193628c6e9991dee84
elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2025-06-09 12:59:28 +0700

    update data table

--- uptd-pegawai/resources/views/layouts/app.blade.php
+++ uptd-pegawai/resources/views/layouts/app.blade.php
@@ -12,10 +12,11 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="//cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css">
-    <!-- Scripts -->
+
+    <!-- DataTables CSS -->
     <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css">
 
+    <!-- Vite -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
@@ -25,48 +26,38 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+                    <!-- Left Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
                             <a href="{{ route('pegawai.index') }}" class="nav-link">Data Pegawai</a>
-
                         </li>
-
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
+                    <!-- Right Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
+                                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                             @endif
-
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu dropdown-menu-end">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        Logout
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
@@ -82,9 +73,21 @@
             @yield('content')
         </main>
     </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdn.datatables.net/2.3.2/js/dataTables.min.js"></script>
-<script> $(document).ready( function () {
-    let table = new DataTable('#myTable');});</script>
+
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- DataTables -->
+    <script src="https://cdn.datatables.net/2.3.2/js/dataTables.min.js"></script>
+
+    <!-- Inisialisasi DataTables -->
+    <script>
+        $(document).ready(function () {
+            $('#datatable').DataTable();
+        });
+    </script>
+
+    <!-- Script tambahan dari halaman lain -->
+    @stack('scripts')
 </body>
 </html>
--- uptd-pegawai/resources/views/pegawai/index.blade.php
+++ uptd-pegawai/resources/views/pegawai/index.blade.php
@@ -23,7 +23,7 @@
     <div class="card shadow-sm">
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered table-striped">
+                <table class="table table-bordered table-striped" id="datatable">
                     <thead class="table-light">
                         <tr>
                             <th>No.</th>
@@ -46,16 +46,16 @@
                             <td>{{ format_rupiah($item->insentif_kotor) }}</td>
                             <td class="text-center">
                                 {{-- Edit button --}}
-                                <a href="{{ route('pegawai.edit', $item->id) }}" class="btn btn-sm btn-warning" title="Edit">
-                                    <i class="bi bi-pencil-fill"></i>
+                                <a href="{{ route('pegawai.edit', $item->id) }}" class="btn btn-warning" title="Edit">
+                                    <i class="bi bi-pencil-fill"></i> Edit
                                 </a>
 
                                 {{-- Delete form --}}
                                 <form action="{{ route('pegawai.destroy', $item->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Yakin ingin hapus pegawai {{ $item->nama }}?')">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm" title="Hapus">
-                                        <i class="bi bi-trash-fill"></i>
+                                    <button type="submit" class="btn btn-danger" title="Hapus">
+                                        <i class="bi bi-trash-fill"></i> Hapus
                                     </button>
                                 </form>
                             </td>
